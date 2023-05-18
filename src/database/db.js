7f2fd67cba78cf1554e69f6dc56de0f986887040
Muhammad Rafi <88550428+RaxonRafi@users.noreply.github.com>
2023-05-19 00:11:29 +0600

    Docker

--- src/database/db.js
+++ src/database/db.js
@@ -1,15 +1,18 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const db = mysql.createConnection({
 
-    host:process.env.HOST,
-    user:process.env.DB_USERNAME,
-    password:process.env.PASSWORD,
-    database:process.env.DB_NAME
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+  
 });
 db.connect((err)=>{
 
     if(err){
+
         console.log(err);
 
     }else{
